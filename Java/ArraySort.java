0d6262bb3625690700e7159607b50f5a713a59cc
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2021-03-22 23:14:38 +0530

    added some more files

---
+++ Java/ArraySort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = input.nextInt();
+        int[] a = new int[size];
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < size; i++) {
+            a[i] = input.nextInt();
+        }
+        System.out.println("");
+        for (int i = 1; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (a[i] > a[j]) {
+                    int temp;
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(a[i] + ",");
+        }
+    }
+}
